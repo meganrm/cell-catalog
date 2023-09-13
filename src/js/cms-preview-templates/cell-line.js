6d13d3f8d63ae69bef9ec7be173af5106314b246
meganrm <meganrm@gmail.com>
2023-09-13 13:56:05 -0700

    editing pages

--- src/js/cms-preview-templates/cell-line.js
+++ src/js/cms-preview-templates/cell-line.js
@@ -1,7 +1,7 @@
 import React from "react";
 import format from "date-fns/format";
 
-export default class PostPreview extends React.Component {
+export default class CellLinePreview extends React.Component {
   render() {
     const {entry} = this.props;
 
